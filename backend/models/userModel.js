0e46d81756cabaa00f18f57d52ef64e89e8f5496
sandesh333-sw <sandeshpulami333@gmail.com>
2025-08-11 17:21:03 +0100

    Working on creating schemas

---
+++ backend/models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+    },
+    repositories: [
+        {
+            default: [],
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        }
+    ],
+    followedUsers: [
+        {
+            default: [],
+            type: Schema.Types.ObjectId,
+            ref: "Repository",
+        }
+    ],
+    starRepos: [
+        {
+            default: [],
+            type: Schema.Types.ObjectId,
+            ref: "Repository",
+        }
+    ],
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
